6127a31ce18d1ae115e4fccc9b17947bf7147dc6
dzhukov40 <dzhukov40.v@mail.ru>
2024-06-05 18:42:49 +0200

    node.js add fetching data

---
+++ NextJS/youtube-_gosha_dudar_nextjs/app/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+const Header = () => {
+    return(
+        <header>
+          <span>Header</span>
+          <nav>
+            <Link href="/">Home</Link>
+          </nav>
+        </header>
+ 
+    )
+}
+
+export default Header
--- NextJS/youtube-_gosha_dudar_nextjs/app/page.tsx
+++ NextJS/youtube-_gosha_dudar_nextjs/app/page.tsx
@@ -1,7 +1,25 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+async function fetchData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const result = await res.json();
+  return result;
+}
+
+
+export default async function Home() {
+  const posts = await fetchData();
+
   return (
-    <h1>Main page</h1>
+    <div>
+      <h1>Main page</h1>
+      {posts.map((el: { id: number, title: string, body: string; }) => (
+        <div key = {el.id} className="post">
+          <h2>{el.title}</h2>
+          <p>{el.body}</p>
+          <Link href={`/post/` + el.id}>Детальнее</Link> 
+        </div>
+      ))}
+    </div>
   );
 }
