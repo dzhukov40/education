6b11a19ff7c3b1030e57756b8fd03775194a2543
dzhukov40 <dzhukov40.v@mail.ru>
2024-06-09 21:58:30 +0200

    next.js add "src" folder; add Parallel routes example

---
+++ NextJS/youtube-codevolution-nextjs/src/app/complex-dashbord/@revenue/page.tsx
@@ -0,0 +1,5 @@
+import Card from "@/src/components/card";
+
+export default function Revenue() {
+  return <Card>Revenue</Card>;
+}
---
+++ NextJS/youtube-codevolution-nextjs/src/app/complex-dashbord/page.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export default function ComplexDashbord() {
+  return <h1>Complex dashbord</h1>;
+}
